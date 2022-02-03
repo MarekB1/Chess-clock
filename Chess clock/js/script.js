4c1aa8f3c67cf41ee0b1e3e5be4b85b104a402e9
MarekB1 <89343471+MarekB1@users.noreply.github.com>
2022-02-03 20:31:53 +0100

    Update script.js

--- Chess clock/js/script.js
+++ Chess clock/js/script.js
@@ -1,33 +1,61 @@
-// Import time spans
-var min = $("minutes-one"),
-    sec = $("seconds-one");
-
-// Import Buttons
+// Imortovanie tlačítiek
     var playerOne = $("#player-one"),
         playerTwo = $("#player-two"),
         pause = $(".pause"),
         resume = $(".resume"),
         reset = $(".reset");
 
-// Controls
-var isPaused = false; // základná hodnota
 
-// Make timer
-var time  = 300, // čas v sekundách
-    timer = setInterval(() => {
-        if(!isPaused && time >= 0) {
-            // set minutes
-            min.innerHTML = parseInt(time / 60, 10);
-            // set seconds
-            sec.innerHTML = parseInt(time % 60, 10);
+var isPaused = false; // základná hodnota pre pauzu do budúcna 
 
-            time--;
-        }
+// Odpočítavanie hráč 2
+function firstPlayerTimer() {
+    console.log('Player jedna odpočítavanie');
+    var timer2 = '5:00';
+    var interval = setInterval(function() {
+        
+        var timer = timer2.split(':');
+        var minutes = parseInt(timer[0], 10);
+        var seconds = parseInt(timer[1], 10);
+        --seconds;
+        minutes = (seconds < 0) ? --minutes : minutes;
+        seconds = (seconds < 0) ? 59 : seconds;
+        seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+        $('.time-one').html(minutes + ':' + seconds);
+
+        if ((seconds <= 0) && (minutes <= 0)) clearInterval(interval);
+        timer2 = minutes + ':' + seconds;
     }, 1000);
+};
 
-    // Click event to the buttons
+// Odpočítavanie hráč 2
+function secondPlayerTimer() {
+    console.log('Player dva odpočítavanie');
+    var timer2 = '5:00';
+    var interval = setInterval(function() {
+        
+        var timer = timer2.split(':');
+        var minutes = parseInt(timer[0], 10);
+        var seconds = parseInt(timer[1], 10);
+        --seconds;
+        minutes = (seconds < 0) ? --minutes : minutes;
+        seconds = (seconds < 0) ? 59 : seconds;
+        seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+        $('.time-two').html(minutes + ':' + seconds);
+
+        if ((seconds <= 0) && (minutes <= 0)) clearInterval(interval);
+        timer2 = minutes + ':' + seconds;
+    }, 1000);
+};
+/*
+*   Ovládanie
+*/
+    // Hráč jeden
 playerOne.on('click', function(event) {
     console.log('Hráč jedna');
+    firstPlayerTimer();
     playerOne.addClass('start');
     playerOne.removeClass('stop');
     playerTwo.addClass('stop');
@@ -35,8 +63,10 @@ playerOne.on('click', function(event) {
     event.preventDefault();
 });
 
+    // Hráč dva
 playerTwo.on('click', function(event) {
     console.log('Hráč dva');
+    secondPlayerTimer();
     playerTwo.addClass('start');
     playerTwo.removeClass('stop');
     playerOne.addClass('stop');
@@ -45,20 +75,33 @@ playerTwo.on('click', function(event) {
 
 });
 
+    // Pauza
 pause.on('click', function(event) {
     console.log('pauza');
+    playerOne.addClass('overlay');
+    playerTwo.addClass('overlay');
+    pause.addClass('overlay');
     event.preventDefault();
-    isPaused = true;
+    playerOne.prop('disabled', true);
 });
 
+    // Pokračovanie
 resume.on('click', function(event) {
     console.log('resume');
+    playerOne.removeClass('overlay');
+    playerTwo.removeClass('overlay');
+    pause.removeClass('overlay');
     event.preventDefault();
-    isPaused = false;
 });
 
+    // Resetovanie
 reset.on('click', function(event) {
     console.log('reset');
+    playerOne.removeClass('start stop');
+    playerTwo.removeClass('start stop');
+    playerOne.removeClass('overlay');
+    playerTwo.removeClass('overlay');
+    pause.removeClass('overlay');
     event.preventDefault();
-    time = 300;
+
 });
